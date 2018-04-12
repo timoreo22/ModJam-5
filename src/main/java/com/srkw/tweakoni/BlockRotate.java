c656f3d65e0d949ed3126c6a7542ac5b17ae3bce
timoreo22 <timo.oreo34@gmail.com>
2018-04-12 08:54:42 +0200

    First draw for block rotate
    has not been tested yet

---
+++ src/main/java/com/srkw/tweakoni/BlockRotate.java
@@ -0,0 +1,96 @@
+package com.srkw.tweakoni;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.block.Block;
+
+public class BlockRotate {
+	
+	public static int MAX_ID = 1024;
+	public static byte[] rotateType = new byte[MAX_ID];
+	public static final byte[] SIDE_OPPOSITE = { 1, 0, 3, 2, 5, 4 };
+	public static final byte[] SIDE_LEFT = { 4, 5, 5, 4, 2, 3 };
+	
+	public static final class RotationType {
+
+		public static final int STAIRS = 1;
+		public static final int SLAB = 2;
+		public static final int CHEST = 3;
+
+		private RotationType() {
+
+		}
+	}
+
+	static {
+
+		rotateType[Block.getIdFromBlock(Blocks.STONE_SLAB)] = RotationType.SLAB;
+		rotateType[Block.getIdFromBlock(Blocks.WOODEN_SLAB)] = RotationType.SLAB;
+		rotateType[Block.getIdFromBlock(Blocks.STONE_SLAB2)] = RotationType.SLAB;
+		rotateType[Block.getIdFromBlock(Blocks.PURPUR_SLAB)] = RotationType.SLAB;
+
+		rotateType[Block.getIdFromBlock(Blocks.TRAPPED_CHEST)] = RotationType.CHEST;
+		rotateType[Block.getIdFromBlock(Blocks.CHEST)] = RotationType.CHEST;
+
+		rotateType[Block.getIdFromBlock(Blocks.OAK_STAIRS)] = RotationType.STAIRS;
+		rotateType[Block.getIdFromBlock(Blocks.STONE_STAIRS)] = RotationType.STAIRS;
+		rotateType[Block.getIdFromBlock(Blocks.BRICK_STAIRS)] = RotationType.STAIRS;
+		rotateType[Block.getIdFromBlock(Blocks.STONE_BRICK_STAIRS)] = RotationType.STAIRS;
+		rotateType[Block.getIdFromBlock(Blocks.NETHER_BRICK_STAIRS)] = RotationType.STAIRS;
+		rotateType[Block.getIdFromBlock(Blocks.SANDSTONE_STAIRS)] = RotationType.STAIRS;
+		rotateType[Block.getIdFromBlock(Blocks.SPRUCE_STAIRS)] = RotationType.STAIRS;
+		rotateType[Block.getIdFromBlock(Blocks.BIRCH_STAIRS)] = RotationType.STAIRS;
+		rotateType[Block.getIdFromBlock(Blocks.JUNGLE_STAIRS)] = RotationType.STAIRS;
+		rotateType[Block.getIdFromBlock(Blocks.QUARTZ_STAIRS)] = RotationType.STAIRS;
+		rotateType[Block.getIdFromBlock(Blocks.RED_SANDSTONE_STAIRS)] = RotationType.STAIRS;
+		rotateType[Block.getIdFromBlock(Blocks.PURPUR_STAIRS)] = RotationType.STAIRS;
+	}
+
+
+//Rotating
+public static IBlockState rotateVanillaBlock(World world, IBlockState state, BlockPos pos) {
+	int bId = Block.getIdFromBlock(state.getBlock()), bMeta = state.getBlock().getMetaFromState(state);
+    Block block = state.getBlock();
+	if(canRotate(state.getBlock())){
+        
+        switch (rotateType[bId]) {
+            case 1:
+                return block.getStateFromMeta(++bMeta % 8);
+            case 2:
+                return block.getStateFromMeta((bMeta + 8) % 16);
+            case 3:
+                BlockPos offsetPos;
+                for (EnumFacing facing : EnumFacing.HORIZONTALS) {
+                    offsetPos = pos.offset(facing);
+                    if (isEqual(world.getBlockState(offsetPos).getBlock(), state.getBlock())) {
+                        world.setBlockState(offsetPos, state.getBlock().getStateFromMeta(SIDE_OPPOSITE[bMeta]), 1);
+                        return block.getStateFromMeta(SIDE_OPPOSITE[bMeta]);
+                    }
+                }
+                return block.getStateFromMeta(SIDE_LEFT[bMeta]);
+            default:
+                return block.getStateFromMeta(bMeta);
+        }
+    }
+	return block.getStateFromMeta(bMeta);
+    }
+    public static boolean canRotate(Block block) {
+
+        return Block.getIdFromBlock(block) < MAX_ID ;
+}		//note maybe add && rotateType[Block.getIdFromBlock(block)] != 0
+    public static boolean isEqual(Block blockA, Block blockB) {
+
+		if (blockA == blockB) {
+			return true;
+		}
+		if (blockA == null | blockB == null) {
+			return false;
+		}
+		return blockA.equals(blockB) || blockA.isAssociatedBlock(blockB);
+}
+
+
+}
--- src/main/java/com/srkw/tweakoni/events/ClientEvents.java
+++ src/main/java/com/srkw/tweakoni/events/ClientEvents.java
@@ -1,30 +1,41 @@
 package com.srkw.tweakoni.events;
 
+import com.srkw.tweakoni.BlockRotate;
 import com.srkw.tweakoni.network.PacketHandler;
 import com.srkw.tweakoni.network.PacketSendLoc;
 import com.srkw.tweakoni.proxy.ClientProxy;
 import com.srkw.tweakoni.utils.RayTrace;
 import com.srkw.tweakoni.utils.handlers.ConfigHandler;
+
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemElytra;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import scala.Console;
+import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod.EventBusSubscriber
 public class ClientEvents {
 
     private static Minecraft mc = Minecraft.getMinecraft();
-
+    private static World world = mc.world;
+    
     @SubscribeEvent
     public static void onRightClick(PlayerInteractEvent.RightClickBlock event) {
         if (event.getWorld().isRemote) {
@@ -68,4 +79,28 @@ public class ClientEvents {
             }
         }
     }
+
+
+@SideOnly(Side.CLIENT)
+
+@SubscribeEvent(priority=EventPriority.NORMAL, receiveCanceled=true)
+public static void onEvent(KeyInputEvent event)
+{
+    // make local copy of key binding 
+    KeyBinding rotate = ClientProxy.block_rotate;
+    int distance = 64;
+    if (rotate.isPressed()) 
+    {
+     EntityPlayer player = world.getPlayerEntityByName(mc.getSession().getUsername());
+     Vec3d vec3 = player.getPositionVector();
+     Vec3d vec3a = player.getLook(1.0F);
+     Vec3d vec3b = vec3.addVector(vec3a.x * distance, vec3a.y * distance, vec3a.z * distance);
+     RayTraceResult result = world.rayTraceBlocks(vec3, vec3b);
+     BlockPos bp = result.getBlockPos();
+     IBlockState state = world.getBlockState(bp);
+     BlockRotate.rotateVanillaBlock(world, state, bp);
+
+    }
+}
+
 }
